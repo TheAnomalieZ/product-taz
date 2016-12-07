3f74c73c35ba18ba95534e40af3bde7ec11653c5
VIthulan <mvvithulan@gmail.com>
2016-12-07 14:31:04 +0530

    Adding getAttributeValues function

--- components/org.taz.commons/src/main/java/org/taz/commons/parser/JFRParser.java
+++ components/org.taz.commons/src/main/java/org/taz/commons/parser/JFRParser.java
@@ -1,9 +1,11 @@
 package org.taz.commons.parser;
 
+import org.taz.commons.exceptions.AttributeNotFoundException;
 import org.taz.commons.exceptions.EventNotFoundException;
 import org.taz.commons.parser.util.EventNode;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface JFRParser {
 
@@ -23,7 +25,7 @@ public interface JFRParser {
      * Get attribute list
      * @param eventName Event name that is required to get
      * @return EventNode object with attributes
-     * @throws EventNotFoundException throws if eventName is not found
+     * @throws EventNotFoundException thrown if eventName is not found in JFR
      */
     public EventNode getJFRAttributes(String eventName) throws EventNotFoundException;
 
@@ -32,5 +34,15 @@ public interface JFRParser {
      * @param path file path
      */
     public void initParser(String path);
-    //public
+
+    /**
+     * Get attribute values from an event
+     * @param eventName Event from where attributes will be received
+     * @param attributes List of attributes that are required
+     * @return Map contains Attribute name and value of list contains attribute values
+     * @throws EventNotFoundException thrown if given event name is not found in JFR
+     * @throws AttributeNotFoundException thrown if given attribute is not found in given event
+     */
+    public Map<String,ArrayList<Object>> getAttributeValues(String eventName, ArrayList<String> attributes)
+            throws EventNotFoundException, AttributeNotFoundException;
 }
