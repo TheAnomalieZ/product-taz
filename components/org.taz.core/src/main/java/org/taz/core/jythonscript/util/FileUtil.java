3d28d53cd0e2c6fc5a86582302eba778040aa2a3
linganesan <linganesan.12@cse.mrt.ac.lk>
2017-02-03 01:03:05 -0500

    extend jythonscript executor

---
+++ components/org.taz.core/src/main/java/org/taz/core/jythonscript/util/FileUtil.java
@@ -0,0 +1,84 @@
+package org.taz.core.jythonscript.util;
+
+import java.io.*;
+
+/**
+ * Provides static file operations, such as converting a {@link String} or {@link File} to an {@link InputStream}.
+ *
+ */
+public class FileUtil {
+
+    /**
+     * Attempts to convert the given {@code object} to an {@link InputStream}. If the object cannot be converted, throws
+     * an {@link Exception}.
+     *
+     * Current supported object conversions include:
+     * <ul>
+     *     <li>{@link String} - as a fully qualified file path</li>
+     *     <li>{@link File}</li>
+     * </ul>
+     *
+     * @param object the object to convert to an input stream
+     * @return a new {@link InputStream} created from the given {@code object}
+     * @throws IOException when the given Object cannot be converted to a FileInputStream
+     */
+    public static InputStream getFileInputStream(Object object) throws IOException {
+        if (object instanceof String) {
+            return new FileInputStream((String) object);
+        } else if (object instanceof File) {
+            return new FileInputStream((File) object);
+        }
+
+        throw new IOException("Could not convert the given object to an InputStream. object=[" + object + "]");
+    }
+
+    /**
+     * Reads the entire contents of the given {@link File} to a {@link String}. Defaults to the UTF-8 character encoding.
+     *
+     * @param file the {@link File} to read the contents of
+     * @return a new {@link String} that contains the contents of the given file
+     * @throws IOException when the given File cannot be found or read from
+     */
+    public static String readFully(File file) throws IOException {
+        if (file == null) {
+            return "";
+        }
+
+        return readFully(new FileInputStream(file), "UTF-8");
+    }
+
+    /**
+     * Reads the entire contents of the given {@link InputStream} to a {@link String}.
+     *
+     * @param inputStream the {@link InputStream} to read the contents of
+     * @param encoding the character encoding that the resulting {@link String} should be (i.e. UTF-8)
+     * @return a new {@link String} that contains the contents of the given stream
+     * @throws IOException when the given InputStream cannot be read from
+     */
+    public static String readFully(InputStream inputStream, String encoding) throws IOException {
+        return new String(readFully(inputStream), encoding);
+    }
+
+    /**
+     * Reads the entire contents of the given {@link InputStream} to a new byte array.
+     *
+     * @param inputStream the {@link InputStream} to read the contents of
+     * @return a new array of bytes that contains the contents of the given stream
+     * @throws IOException when the given InputStream cannot be read from
+     */
+    private static byte[] readFully(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+
+        while ((length = inputStream.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
+        }
+
+        return baos.toByteArray();
+    }
+
+    // Don't allow this class to be instantiated
+    private FileUtil() { }
+
+}
---
+++ components/org.taz.core/src/main/java/org/taz/core/jythonscript/util/StringUtil.java
@@ -0,0 +1,32 @@
+package org.taz.core.jythonscript.util;
+
+/**
+ * Provides basic operations for {@link String}s.
+ *
+ */
+public class StringUtil {
+
+    /**
+     * Determines if the given {@link String} is null or empty.
+     *
+     * @param value the String to check
+     * @return true if the String is null or empty; false otherwise
+     */
+    public static boolean isBlank(String value) {
+        return value == null || value.length() == 0 || value.trim().length() == 0;
+    }
+
+    /**
+     * Determines if the given {@link String} is not null or empty.
+     *
+     * @param value the String to check
+     * @return true if the String is not null or empty; false otherwise
+     */
+    public static boolean isNotBlank(String value) {
+        return value != null && value.trim().length() > 0;
+    }
+
+    // Don't allow this class to be instantiated
+    private StringUtil() { }
+
+}
