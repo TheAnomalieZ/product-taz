c19b9985ce9779b2802c7e48a63d918fab5898aa
VIthulan <mvvithulan@gmail.com>
2017-02-19 14:03:50 +0530

    adding properties.java

---
+++ components/org.taz.core/src/main/java/org/taz/core/clustering/util/Properties.java
@@ -0,0 +1,10 @@
+package org.taz.core.clustering.util;
+
+/**
+ * Created by vithulan on 2/19/17.
+ */
+public class Properties {
+    public static final String ATTRIBUTE_TABLE_FILEPATH = "files/clustering/attribute_tables/";
+    public static final String MINPOINT_TEST_FILEPATH = "files/clustering/minpoint_test/";
+    public static final String ANOMALY_SCORES_FILEPATH = "files/clustering/anomaly_scores/";
+}
