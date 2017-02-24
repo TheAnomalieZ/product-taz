ea31665ae767a83bb409cd6faca134272588cd19
VIthulan <mvvithulan@gmail.com>
2017-02-24 19:09:42 +0530

    Fixing relative path bug

--- components/org.taz.core/src/main/java/org/taz/core/clustering/util/Properties.java
+++ components/org.taz.core/src/main/java/org/taz/core/clustering/util/Properties.java
@@ -4,7 +4,8 @@ package org.taz.core.clustering.util;
  * Created by vithulan on 2/19/17.
  */
 public class Properties {
-    public static final String ATTRIBUTE_TABLE_FILEPATH = "/home/suve/product-taz/files/clustering/attribute_tables/";
-    public static final String MINPOINT_TEST_FILEPATH = "/home/suve/product-taz/files/clustering/minpoint_test/";
-    public static final String ANOMALY_SCORES_FILEPATH = "/home/suve/product-taz/files/clustering/anomaly_scores/";
+    public static final String SYSTEM_PATH = System.getProperty("user.dir");
+    public static final String ATTRIBUTE_TABLE_FILEPATH = SYSTEM_PATH+"/files/clustering/attribute_tables/";
+    public static final String MINPOINT_TEST_FILEPATH = SYSTEM_PATH+"/files/clustering/minpoint_test/";
+    public static final String ANOMALY_SCORES_FILEPATH = SYSTEM_PATH+"/files/clustering/anomaly_scores/";
 }
