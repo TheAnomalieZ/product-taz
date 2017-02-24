b528de4755490c7205df755cadc363da45737426
linganesan <linganesan.12@cse.mrt.ac.lk>
2017-02-24 02:37:34 -0500

    add skelton for hmm test

---
+++ components/org.taz.core/src/main/java/org/taz/core/tests/HMMTest.java
@@ -0,0 +1,36 @@
+package org.taz.core.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Maninesan on 2/24/17.
+ */
+public class HMMTest {
+    @Test
+    public void generateScoreSeries() throws Exception {
+
+    }
+
+    @Test
+    public void generateAnomalyLabels() throws Exception {
+
+    }
+
+    @Test
+    public void getPercentileValue() throws Exception {
+
+    }
+
+    @Test
+    public void generateThreshold() throws Exception {
+
+    }
+
+    @Test
+    public void getAnomlayTimes() throws Exception {
+
+    }
+
+}
