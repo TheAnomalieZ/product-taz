94c7455c36abfa83d4ed7ceba3646fd1512df84e
linganesan <linganesan.12@cse.mrt.ac.lk>
2017-03-05 04:15:15 -0500

    add more tests

--- components/org.taz.commons/src/main/java/org/taz/commons/tests/JFRParserV18Test.java
+++ components/org.taz.commons/src/main/java/org/taz/commons/tests/JFRParserV18Test.java
@@ -1,18 +1,29 @@
 package org.taz.commons.tests;
 
 
+import au.com.bytecode.opencsv.CSVReader;
 import org.taz.commons.constants.JFRConstants;
 import org.taz.commons.parser.JFRParserV18;
+import org.taz.commons.parser.events.JVMInformationEvent;
 import org.taz.commons.parser.util.EventNode;
+import org.taz.commons.util.JFRReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 /**
  * Created by Maninesan on 2/23/17.
  */
 public class JFRParserV18Test {
+    String jfrPath;
     @org.testng.annotations.BeforeMethod
     public void setUp() throws Exception {
+            jfrPath ="/home/garth/product-taz/components/org.taz.commons/src/main/java/org/taz/commons/tests/normal.jfr";
 
     }
 
@@ -21,31 +32,87 @@ public class JFRParserV18Test {
 
     }
 
+
+
     @org.testng.annotations.Test
-    public void testGetMemoryStates1() throws Exception {
+    public void testGetMemoryStates() throws Exception {
+        //generate gc states from parser
+        ArrayList<Integer> testlist = new JFRParserV18(jfrPath).getMemoryStates();
+
+        //read gc states from CSV
+        String statespath = TestConstants.states;
+        CSVReader reader = null;
+        ArrayList<Integer> states = new ArrayList<Integer>();
+        try {
+            reader = new CSVReader(new FileReader( statespath));
+            String [] nextLine;
+            int score;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                score = Integer.parseInt(nextLine[0]);
+                states.add(score);
+            }
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        //check both
+        assertEquals(states,testlist);
+    }
+
+
+
+    @org.testng.annotations.Test
+    public void testGetHeapTimeSeries() throws Exception {
+        //generate heap time series from parser
+        ArrayList<Double> testlist = JFRReader.convertGCTimeSeries(new JFRParserV18(jfrPath).getGCTimeSeries());
+
+        //read heap time series form CSV
+        String heaptimeseriespaht = TestConstants.heaptimeseries;
+        CSVReader reader = null;
+        ArrayList<Double> heaptimeseries = new ArrayList<Double>();
+        try {
+            reader = new CSVReader(new FileReader( heaptimeseriespaht));
+            String [] nextLine;
+            Double score;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                score = Double.parseDouble(nextLine[0]);
+                heaptimeseries.add(score);
+            }
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        //check both
+        assertEquals(heaptimeseries,testlist);
+    }
+
+    /**
+     * check to not null return
+     * @throws Exception
+     */
+    @org.testng.annotations.Test
+    public void testGetJVMInformationEvent() throws Exception {
+        JVMInformationEvent je =new JFRParserV18(jfrPath).getJVMInformationEvent();
+        assertNotNull(je);
+    }
+
+    
+
+    @org.testng.annotations.Test
+    public void testGetHotMethods() throws Exception {
 
     }
 
-    @org.testng.annotations.Test
-    public void testGetHeapTimeSeries1() throws Exception {
 
-    }
 
-    @org.testng.annotations.Test
-    public void testGetPauseTimeSeries1() throws Exception {
-
-    }
-
-    @org.testng.annotations.Test
-    public void testGetJVMInformationEvent1() throws Exception {
-
-    }
-
-    @org.testng.annotations.Test
-    public void testGetHotMethods1() throws Exception {
-
-    }
-
+    /**
+     * Test the CPULOAD event attributes
+     * @throws Exception
+     */
     @org.testng.annotations.Test
     public void testGetJFRAttributes() throws Exception {
         String[] a = new String[]{"(startTime)", "(endTime)", "(duration)",
@@ -54,7 +121,7 @@ public class JFRParserV18Test {
                 ,"(eventTypePath)"
                 ,"jvmUser","jvmSystem", "machineTotal"};
 
-        EventNode en =  new JFRParserV18("/home/garth/FYP/product-taz/components/org.taz.commons/src/main/java/org/taz/commons/tests/normal.jfr").getJFRAttributes(JFRConstants.CPULOAD);
+        EventNode en =  new JFRParserV18(jfrPath).getJFRAttributes(JFRConstants.CPULOAD);
         Object[] arrayList= en.getAttributes().toArray();
         assertEquals(a,arrayList);
 
---
+++ components/org.taz.commons/src/main/java/org/taz/commons/tests/TestConstants.java
@@ -0,0 +1,9 @@
+package org.taz.commons.tests;
+
+/**
+ * Created by Maninesan on 2/23/17.
+ */
+public class TestConstants {
+    public final static String heaptimeseries = "/home/garth/product-taz/components/org.taz.commons/src/main/java/org/taz/commons/tests/normal_heap.csv";
+    public final static String states = "/home/garth/product-taz/components/org.taz.commons/src/main/java/org/taz/commons/tests/normal_states.csv";
+}
