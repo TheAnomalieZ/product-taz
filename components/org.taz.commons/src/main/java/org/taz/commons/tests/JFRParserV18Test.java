78638a8c2c9d80e51766916c62738068cb442487
linganesan <linganesan.12@cse.mrt.ac.lk>
2017-03-07 01:53:39 -0500

    refine test in commons

--- components/org.taz.commons/src/main/java/org/taz/commons/tests/JFRParserV18Test.java
+++ components/org.taz.commons/src/main/java/org/taz/commons/tests/JFRParserV18Test.java
@@ -2,7 +2,9 @@ package org.taz.commons.tests;
 
 
 import au.com.bytecode.opencsv.CSVReader;
+import org.mockito.Mock;
 import org.taz.commons.constants.JFRConstants;
+import org.taz.commons.exceptions.EventNotFoundException;
 import org.taz.commons.parser.JFRParserV18;
 import org.taz.commons.parser.events.JVMInformationEvent;
 import org.taz.commons.parser.util.EventNode;
@@ -13,32 +15,107 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 /**
  * Created by Maninesan on 2/23/17.
  */
+
+
 public class JFRParserV18Test {
-    String jfrPath;
+
+    @Mock
+    EventNode eventNode = mock(EventNode.class);
+
+    private JFRParserV18 jfrParser;
+
+    private final String jfrPath = "/home/tests/normal.jfr";
+    private final String statesPath = "/home/tests/normal_states.csv";
+    private final String eventName1 = JFRConstants.CPULOAD;
+    private final String eventName2 = JFRConstants.HEAPSUMMARY;
+    private final String faulteventName = "Excetiion check";
+
+
     @org.testng.annotations.BeforeMethod
     public void setUp() throws Exception {
-            jfrPath ="/home/garth/product-taz/components/org.taz.commons/src/main/java/org/taz/commons/tests/normal.jfr";
+        jfrParser =new JFRParserV18(jfrPath);
+
+        when(eventNode.getDescription()).thenReturn("");
+    }
+
+
+    /**
+     * Test the CPU Load event attributes
+     * Test Path [1,2,4,5]
+     * @throws Exception
+     */
+    @org.testng.annotations.Test
+    public void testGetJFRAttributes1() throws Exception {
+        //create expected String array
+        String[] expectedArray = new String[]{"(startTime)", "(endTime)", "(duration)",
+                "(producer)","(producerURI)"
+                ,"(eventType)"
+                ,"(eventTypePath)"
+                ,"jvmUser","jvmSystem", "machineTotal"};
+
+        //call CPU Load event node
+        EventNode en =  jfrParser.getJFRAttributes(eventName1);
+
+        //get the actual result from CPUload node
+        Object[] outputArray= en.getAttributes().toArray();
+
+        //test
+        assertEquals(outputArray,expectedArray);
+    }
+
+    /**
+     * Test the Heap Summary event attributes
+     *  Test Path [1,2,3,5]
+     * @throws Exception
+     */
+
+    @org.testng.annotations.Test
+    public void testGetJFRAttributes3() throws Exception {
+        //call the expected result from mock event node object
+        String expectedDescription = eventNode.getDescription();
+
+        //call Heap summary event node
+        EventNode en =jfrParser.getJFRAttributes(eventName2);
+
+        //get the actual description result
+        String description = en.getDescription();
+
+        //test
+        assertEquals(description,expectedDescription);
 
     }
 
-    @org.testng.annotations.AfterMethod
-    public void tearDown() throws Exception {
+    /**
+     * Test the expected exception in getJFRAttributes method
+     *  Test Path [1,6]
+     * @throws Exception
+     */
 
+    @org.testng.annotations.Test(expectedExceptions = EventNotFoundException.class)
+    public void testGetJFRAttributes2() throws Exception {
+        //call faulty event name to throw exception
+        jfrParser.getJFRAttributes(faulteventName);
     }
 
+    /**
+     * check the getMemoryStates
+     * @throws Exception
+     */
     @org.testng.annotations.Test
     public void testGetMemoryStates() throws Exception {
         //generate gc states from parser
         ArrayList<Integer> testlist = new JFRParserV18(jfrPath).getMemoryStates();
 
         //read gc states from CSV
-        String statespath = TestConstants.states;
+        String statespath = statesPath;
         CSVReader reader = null;
         ArrayList<Integer> states = new ArrayList<Integer>();
         try {
@@ -59,9 +136,11 @@ public class JFRParserV18Test {
         assertEquals(states,testlist);
     }
 
-
+    /**
+     * check the getHeapTimeSeries method
+     * @throws Exception
+     */
     @org.testng.annotations.Test
-
     public void testGetHeapTimeSeries() throws Exception {
         //generate heap time series from parser
         ArrayList<Double> testlist = JFRReader.convertGCTimeSeries(new JFRParserV18(jfrPath).getGCTimeSeries());
@@ -98,31 +177,36 @@ public class JFRParserV18Test {
         assertNotNull(je);
     }
 
-    
-
-    @org.testng.annotations.Test
-    public void testGetHotMethods() throws Exception {
-
-    }
-
-    /**
-     * Test the CPULOAD event attributes
-     * @throws Exception
-     */
-    @org.testng.annotations.Test
-    public void testGetJFRAttributes() throws Exception {
-        String[] a = new String[]{"(startTime)", "(endTime)", "(duration)",
-                "(producer)","(producerURI)"
-                ,"(eventType)"
-                ,"(eventTypePath)"
-                ,"jvmUser","jvmSystem", "machineTotal"};
-
-        EventNode en =  new JFRParserV18(jfrPath).getJFRAttributes(JFRConstants.CPULOAD);
-        Object[] arrayList= en.getAttributes().toArray();
-        assertEquals(a,arrayList);
 
 
-    }
+
+
+
+
+//    @Test
+//    public void mockFinalClassTest() {
+//        AFinalClass tested = PowerMockito.mock(AFinalClass.class);
+//
+//        final String testInput = "A test input";
+//        final String mockedResult = "Mocked final echo result - " + testInput;
+//        Mockito.when(tested.echoString(testInput)).thenReturn(mockedResult);
+//
+//        // Assert the mocked result is returned from method call
+//        Assert.assertEquals(tested.echoString(testInput), mockedResult);
+//    }
+//
+//    @Test
+//    public void mockStaticClassTest() {
+//        PowerMockito.mockStatic(AStaticClass.class);
+//
+//        final String testInput = "A test input";
+//        final String mockedResult = "Mocked static echo result - " + testInput;
+//        Mockito.when(AStaticClass.echoString(testInput)).thenReturn(mockedResult);
+//
+//        // Assert the mocked result is returned from method call
+//        Assert.assertEquals(AStaticClass.echoString(testInput), mockedResult);
+//    }
+
 
 
 }
